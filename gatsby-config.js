66f52b0a32088f3bbcd81b425704107aee80f793
Jack Moore <jtmthf@mst.edu>
2019-04-28 12:32:01 -0500

    netlify enhancements

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,13 @@ module.exports = {
       },
     },
     `gatsby-plugin-webpack-bundle-analyser-v2`,
+    {
+      resolve: `gatsby-plugin-netlify-cache`,
+      options: {
+        extraDirsToCache: [`.linaria-cache`],
+      },
+    },
+    `gatsby-plugin-netlify`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
